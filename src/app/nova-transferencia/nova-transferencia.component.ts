2fee11d972878cc2352b207ff5b8b09c690764e7
Diego Minelli <diego.minelli@hotmail.com>
2022-03-27 12:44:51 -0300

    fazendo a interpolacao dos dados

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,24 +3,26 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent implements OnInit {
-
   @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   transferir() {
     console.log('Solicitado nova transferência');
-    const valorEmitir = {valor: this.valor, destino: this.destino}
-    this.aoTransferir.emit(valorEmitir)
-    
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos()
   }
 
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
