d667a19f5bb2816302125d0962e6958f3eb0ec57
Diego Minelli <diego.minelli@hotmail.com>
2022-03-27 12:20:17 -0300

    exportando dados no componente

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent implements OnInit {
+
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
@@ -16,8 +18,8 @@ export class NovaTransferenciaComponent implements OnInit {
   }
   transferir() {
     console.log('Solicitado nova transferência');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+    const valorEmitir = {valor: this.valor, destino: this.destino}
+    this.aoTransferir.emit(valorEmitir)
     
   }
 
